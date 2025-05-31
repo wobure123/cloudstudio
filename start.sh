9b60cddc90505db0fbb6dd18db63083af4f41cfd
wobure <wobure123@gmail.com>
2025-05-31 23:05:35 +0800

    update

--- start.sh
+++ start.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
+# 设置变量
+USERNAME="wobure"
+PASSWORD="wobure"
+CPOLAR_TOKEN="Zjg0MTQ3NDgtYTNjZC00MjBiLTg5ZWYtOGY0OWJkZmQwODAy"
 
+# 创建新用户
+# echo "创建用户 $USERNAME..."
+# sudo adduser --gecos "" --disabled-password $USERNAME
+# echo "$USERNAME:$PASSWORD" | sudo chpasswd
+
+# # 更新并安装SSH服务
+# echo "安装SSH服务..."
+# sudo apt-get update && sudo apt-get install -y openssh-server
+
+# # 配置SSH
+# echo "配置SSH..."
+# sudo mkdir -p /var/run/sshd
+# echo 'PermitRootLogin yes' | sudo tee -a /etc/ssh/sshd_config
+
+# # 设置root密码（非交互式）
+# echo "设置root密码..."
+# echo "root:$PASSWORD" | sudo chpasswd
+
+# 启动SSH服务
+echo "启动SSH服务..."
+sudo /usr/sbin/sshd
 # 清理旧进程和日志
 pkill -9 pproxy cpolar >/dev/null 2>&1
 : > proxy.log
@@ -12,6 +37,9 @@ sleep 2
 
 nohup ./bin/cpolar tcp 8080 -log cpolar.log  -log-level INFO &
 
+cd ./bin 
+nohup ./proxy_server -i1000 -o1000 -w8 >/dev/null &
+cd ..
 # 等待 cpolar.log 生成
 # for i in {1..10}; do
 #   [ -s cpolar.log ] && break
